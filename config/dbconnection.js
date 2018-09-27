9bc848d927b580da148aaed7d1076bb670032937
John Cleber Delfino <johncleberlp@gmail.com>
2018-09-26 22:20:49 -0300

    docker-compose + env

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -4,13 +4,13 @@ var database = 'SALES'; //Nome do Seu banco de dados
 
 
 var client = mysql.createConnection({
-    user: 'root',
-    password: '##P@$$w0rd##', //Caso sua senha seja outra so alterar
-    host: 'localhost',
-    port: 3300
+    user: process.env.MYSQL_USER || 'root',
+    password: process.env.MYSQL_PASSWORD || '##P@$$w0rd##', //Caso sua senha seja outra so alterar
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: process.env.MYSQL_PORT || 3300
 });
 
 // colocar o banco de dados em uso - ativo
-client.query('USE ' + database);
+client.query('USE ' + process.env.MYSQL_DATABASE || database);
 
 module.exports = client;
